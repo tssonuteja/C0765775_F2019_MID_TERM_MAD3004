3830432b505466d108b751890e98bf737f856665
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:23:51 -0400

    20

--- C0765775_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -2,6 +2,11 @@
 
 import Foundation
 class Mobile:Bill{
-    
+    var manufacturerName:String?
+    var plan:String?
+    var mobileNumber:String?
+    var internetUsed:Float?
+    var minutesUsed:Int?
+    var billAmount : Float?
     
 }
