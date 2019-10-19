737f5592d66fb42a406aa37d7ca9ba78086626e8
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 19:29:24 -0400

    59

--- C0765775_F2019_MID_TERM_MAD3004/main.swift
+++ C0765775_F2019_MID_TERM_MAD3004/main.swift
@@ -6,19 +6,19 @@ var result = date.dateformatter()
 var custDictionary = Dictionary<String,Customers>()
 var totalBill : Float
 
-var i = Internet(providerName: "Fido", gbUsed: 26.5, billId: "M0FD1", billDate: result, billAmount: 100, billType: Bill.billing.Internet)
+var net = Internet(providerName: "Fido", gbUsed: 26.5, billId: "M0FD1", billDate: result, billAmount: 100, billType: Bill.billing.Internet)
 
 var m = Mobile(manufacturerName: "Samsung", plan: "Summer", mobileNumber: "4172396019", internetUsed: 1.3, minutesUsed: 900, billAmount: 180, billId: "MSAM2", billType:Bill.billing.Mobile, billDate: result)
 
 var h = Hydro(agencyName: "e POWER", unitsConsumed: 201, billAmount: 400, billId: "ePwr2", billType: Bill.billing.Hydro, billDate: result)
-var c = Customers(customerId: "C342", firstName: "Sai teja", lastName: "Sunkari", email: "tssonute@gmail.com", billingDictionary : [i.billId!:i,m.billId!:m,h.billId!:h])
+var c = Customers(customerId: "C342", firstName: "Sai teja", lastName: "Sunkari", email: "tssonute@gmail.com", billingDictionary : [net.billId!:net,m.billId!:m,h.billId!:h])
 
-var i1 = Internet(providerName: "bell", gbUsed: 12, billId: "B0bl1", billDate: result, billAmount: 98, billType: Bill.billing.Internet)
+var net1 = Internet(providerName: "bell", gbUsed: 12, billId: "B0bl1", billDate: result, billAmount: 98, billType: Bill.billing.Internet)
 
 var h1 = Hydro(agencyName: "Thermal POWER", unitsConsumed: 20, billAmount: 100, billId: "Tpe2", billType: Bill.billing.Hydro, billDate: result)
 
-var c1 = Customers(customerId: "C222", firstName: "Shank", lastName: "mitta", email: "saha123@gmail.com", billingDictionary : [i.billId!:i,m.billId!:m,h.billId!:h])
-var c2 = Customers(customerId: "C242", firstName: "joe", lastName: "kunyan", email: "joeqw123@gmail.com", billingDictionary : [i.billId!:i,m.billId!:m,h.billId!:h])
+var c1 = Customers(customerId: "C222", firstName: "Shank", lastName: "mitta", email: "saha123@gmail.com", billingDictionary : [net1.billId!:net1,m.billId!:m,h.billId!:h])
+var c2 = Customers(customerId: "C242", firstName: "joe", lastName: "kunyan", email: "joeqw123@gmail.com", billingDictionary : [net1.billId!:net1,m.billId!:m,h.billId!:h])
 
 
 custDictionary.updateValue(c, forKey: c.customerId!)
@@ -33,7 +33,7 @@ for i in custDictionary.values{
 
 for temp in custDictionary.values{
     
-    for i1 in temp.billingDictionary.values
+    for net1 in temp.billingDictionary.values
     {
         
     }
