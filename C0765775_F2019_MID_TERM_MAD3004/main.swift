e64c7f9aa48197e2f88029a444c09d7fdbe7d8ca
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 19:04:07 -0400

    47

--- C0765775_F2019_MID_TERM_MAD3004/main.swift
+++ C0765775_F2019_MID_TERM_MAD3004/main.swift
@@ -8,3 +8,4 @@ var totalBill : Float
 
 var i = Internet(providerName: "Fido", gbUsed: 26.5, billId: "M0FD1", billDate: result, billAmount: 100, billType: Bill.billing.Internet)
 var m = Mobile(manufacturerName: "Samsung", plan: "Summer", mobileNumber: "4172396019", internetUsed: 1.3, minutesUsed: 900, billAmount: 180, billId: "MSAM2", billType:Bill.billing.Mobile, billDate: result)
+var h = Hydro(agencyName: "e POWER", unitsConsumed: 201, billAmount: 400, billId: "ePwr2", billType: Bill.billing.Hydro, billDate: result)
