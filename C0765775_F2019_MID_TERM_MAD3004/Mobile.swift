ae1a9c869b1414b6624f55d88f855269e4703510
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:22:40 -0400

    19

--- C0765775_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -3,26 +3,5 @@
 import Foundation
 class Mobile:Bill{
     
-    var pName : String?
-    var gbUsed : Float?
-    var billAmount : Float?
     
-    init(pName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.billing)
-    {
-        self.pName = pName
-        self.gbUsed = gbUsed
-        self.billAmount = billAmount
-        super.init(billId: billId, billDate: billDate, billType: billType)
-}
-    
-    override func iDisplay() {
-        print("*********************************")
-        print("Bill Id : \(billId)")
-        print("Bill Type : \(billType)")
-        print("Bill Date : \(billDate)")
-        print("Provider Name : \(pName)")
-        print("Data Used : \(gbUsed)")
-        print("Bill Amount : \(billAmount)")
-
-    }
 }
