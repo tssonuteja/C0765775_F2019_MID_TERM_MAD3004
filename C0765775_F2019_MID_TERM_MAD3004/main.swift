ac7ad2cd33b7a94df2a181eb7d7da6220970ee85
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:58:36 -0400

    45

--- C0765775_F2019_MID_TERM_MAD3004/main.swift
+++ C0765775_F2019_MID_TERM_MAD3004/main.swift
@@ -2,7 +2,9 @@
 import Foundation
 
 var date = Date()
-var result = date
+var result = date.dateformatter()
 var custDictionary = Dictionary<String,Customers>()
 var totalBill : Float
+
+var i = Internet(providerName: "Fido", gbUsed: 26.5, billId: "M0FD1", billDate: result, billAmount: 100, billType: Bill.billing.Internet)
 
