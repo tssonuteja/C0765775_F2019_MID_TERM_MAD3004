4a37023f660fa3bb3ea902497d13330beb8c0194
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 17:58:37 -0400

    5

--- C0765775_F2019_MID_TERM_MAD3004/Bill.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Bill.swift
@@ -4,9 +4,13 @@ import Foundation
 class Bill{
     var billId : String?
     var billDate : String?
+    var totalBillAmount : Float?
     enum Types {
         case Internet
        case Mobile
         case Hydro
     }
+    
+    var billType : Types?
+    
 }
