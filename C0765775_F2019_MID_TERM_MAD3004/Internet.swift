e74ff7d069f66f25930c1542ead31d791d56490c
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:51:12 -0400

    42

--- C0765775_F2019_MID_TERM_MAD3004/Internet.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Internet.swift
@@ -1,13 +1,13 @@
 
 import Foundation
 class Internet: Bill{
-    var pName : String?
+    var providerName : String?
     var gbUsed : Float?
     var billAmount : Float?
     
-    init(pName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.billing)
+    init(providerName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.billing)
     {
-        self.pName = pName
+        self.providerName = providerName
         self.gbUsed = gbUsed
         self.billAmount = billAmount
         super.init(billId: billId, billDate: billDate, billType: billType)
@@ -18,7 +18,7 @@ class Internet: Bill{
         print("Bill Id : \(billId)")
         print("Bill Type : \(billType)")
         print("Bill Date : \(billDate)")
-        print("Provider Name : \(pName)")
+        print("Provider Name : \(providerName)")
         print("Data Used : \(gbUsed)")
         print("Bill Amount : \(billAmount)")
         
