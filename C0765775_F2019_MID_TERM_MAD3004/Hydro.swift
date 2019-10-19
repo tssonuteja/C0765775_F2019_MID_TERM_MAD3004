c8a38157b904a0748b1ff3a255c95f5fd6f9ed02
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:33:13 -0400

    28

--- C0765775_F2019_MID_TERM_MAD3004/Hydro.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Hydro.swift
@@ -5,4 +5,13 @@ class Hydro: Bill{
     var agency:String?
     var unitsConsumed: Float?
     var billAmount : Float?
+    
+    init(agency:String,unitsConsumed:Float,billAmount : Float,billId : String,billType : Bill.billing,billDate : String)
+    {
+        self.agency=agency
+        self.unitsConsumed=unitsConsumed
+        self.billAmount=billAmount
+        super.init(billId: billId, billDate: billDate, billType: billType)
+        
+    }
 }
