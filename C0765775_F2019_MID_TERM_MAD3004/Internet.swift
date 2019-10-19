8bd011c8e2a2dd930aa618d5fac719d5bea876be
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:22:01 -0400

    17

--- C0765775_F2019_MID_TERM_MAD3004/Internet.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Internet.swift
@@ -4,4 +4,13 @@ class Internet: Bill{
     var pName : String?
     var gbUsed : Float?
     var billAmount : Float?
+    
+    init(pName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.billing)
+    {
+        self.pName = pName
+        self.gbUsed = gbUsed
+        self.billAmount = billAmount
+        super.init(billId: billId, billDate: billDate, billType: billType)
+    }
+    
 }
