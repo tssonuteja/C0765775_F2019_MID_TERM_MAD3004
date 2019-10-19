ce9dfcf6581acbc69418630ab55f652e0ef3126d
tssonuteja <saitejasunkari04@gmail.com>
2019-10-19 18:16:10 -0400

    13

--- C0765775_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0765775_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -7,10 +7,18 @@ class Mobile:Bill{
     var gbUsed : Float?
     var billAmount : Float?
     
-    init(pName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.billing) {
+    init(pName : String, gbUsed : Float, billId : String, billDate : String,billAmount: Float, billType : Bill.billing)
+    {
         self.pName = pName
         self.gbUsed = gbUsed
         self.billAmount = billAmount
         super.init(billId: billId, billDate: billDate, billType: billType)
 }
+    
+    override func iDisplay() {
+        print("***************************************")
+        print("Bill Id : \(billId)")
+        print("Bill Type : \(billType)")
+        print("Bill Date : \(billDate)")
+    }
 }
